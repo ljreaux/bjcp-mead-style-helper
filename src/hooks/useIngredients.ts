b9f3807aa5f5466c57bf0bf17fca6717ab782789
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-07-27 13:05:28 -0500

    updated to use MeadTools DB

--- src/hooks/useIngredients.ts
+++ src/hooks/useIngredients.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import type { Ingredient } from "./useMeadStyle";
+const BASE_URL = "https://meadtools.com";
 
 export const useIngredients = () => {
   return useQuery<Ingredient[]>({
     queryKey: ["ingredients"],
     queryFn: async () => {
-      const res = await fetch("/ingredients.json");
+      const res = await fetch(`${BASE_URL}/api/bjcp-ingredients`);
       if (!res.ok) {
         throw new Error("Failed to fetch ingredient data");
       }
